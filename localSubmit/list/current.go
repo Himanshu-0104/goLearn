387ef8081ad800f8543950687daa00593a67cbda
Himanshu-0104 <hybirar.103@gmail.com>
2026-05-18 14:46:34 +0530

    update: 18May

--- localSubmit/list/current.go
+++ localSubmit/list/current.go
@@ -10,6 +10,18 @@ import (
 
 
 
+// func solve() {
+//     n:=0
+//     fmt.Fscan(in,&n)
+//     for i:=0;i<n;i++{
+//         fmt.Fscan(in,&nums[i])
+//     }
+//     head:=buildList(nums)
+
+//     ans := reverseList(head)
+
+//     printList(ans)
+// }
 
 
 
@@ -19,41 +31,33 @@ import (
 
 
 
+// type ListNode struct {
+//     Val int
+//     Next *ListNode
+// }
 
+// func buildList(nums []int) *ListNode {
 
+//     var temp *Node
+//     temp = &ListNode{} //empty list 
+//     curr := temp
 
+//     for i:=0;i<len(nums);i++ {
+//         curr.Next = &ListNode{Val: nums[i]}
+//         curr=curr.Next
+//     }
+//     return temp.Next //bcz first node created at curr.Next
+// }
 
+// func printList(head *ListNode) []int{
+//     nums:=make([]int,0)
+//     for head != nil {
+//         nums = append(nums,head.Val)
+//         head= head.Next
+//     }
+//     return nums
+// }
 
-func Merge(intervals [][]int) [][]int {
-    // //bruteforce n^2(each with other lefts)
-    n:=len(intervals)
-    
-    ans:=make([][]int,0)
-    visit:=make([]bool,n) //total no. of pairs
-    for i:=0;i<n;i++ {
-        if visit[i] {continue} // to skip already merged by innerloop
-        visit[i]=true
-        start:=intervals[i][0]
-        end:=intervals[i][1]
-
-        for j:=0; j<n; j++ {
-            if visit[j] {continue}
-            cstart:=intervals[j][0]
-            cend:=intervals[j][1]
-            //condition of overlap
-            if max(cstart,start) <= min(cend,end) {
-                start = min(cstart,start)
-                end = max(cend,end)
-                visit[j]=true //merged to skip in outerloop
-            }
-            
-        }
-        ans=append(ans,[]int{start,end})
-    }
-    return ans
-
-    // //optimal nlogn(sort acc to starting point)
-}
 
 
 
--- localSubmit/list/linkedList.go
+++ localSubmit/list/linkedList.go
@@ -7,20 +7,28 @@ type ListNode struct {
     Next *ListNode
 }
 
-// create linked list from input
 func buildList(nums []int) *ListNode {
-    dummy := &ListNode{}
-    cur := dummy
 
-    for _, v := range nums {
-        cur.Next = &ListNode{Val: v}
-        cur = cur.Next
+    var temp *ListNode
+    temp = &ListNode{} //empty list 
+    curr := temp
+
+    for i:=0;i<len(nums);i++ {
+        curr.Next = &ListNode{Val: nums[i]}
+        curr=curr.Next
     }
-
-    return dummy.Next
+    return temp.Next //bcz first node created at curr.Next
 }
 
-// print linked list
+func listToSlice (head *ListNode) []int{
+    nums:=make([]int,0)
+    for head != nil {
+        nums = append(nums,head.Val)
+        head= head.Next
+    }
+    return nums
+}
+
 func printList(head *ListNode) {
     for head != nil {
         fmt.Fprint(out, head.Val)
@@ -31,3 +39,17 @@ func printList(head *ListNode) {
     }
     fmt.Fprintln(out)
 }
+
+
+// // idomatic way
+// func buildList(nums []int) *ListNode {
+//     dummy := &ListNode{}
+//     cur := dummy
+
+//     for _, v := range nums {
+//         cur.Next = &ListNode{Val: v}
+//         cur = cur.Next
+//     }
+
+//     return dummy.Next
+// }
